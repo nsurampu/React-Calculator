3ca946bb9cec18b5ef3bade52565406b02eb92ee
Naren Surampudi <nsurampu@gmail.com>
2019-05-20 15:39:02 +0530

    Added core files

---
+++ App.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ResultComponent from './result-component.js';
+import InputComponent from './input-component.js';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: '0'
+    }
+  }
+
+  onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.clear()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+  calculate = () => {
+      try {
+          this.setState({
+                result: (eval(this.state.result) || "" ) + ""
+            })
+        }
+    catch (e) {
+            this.setState({
+                result: "Error"
+            })
+
+        }
+  };
+
+  reset = () => {
+    this.setState({
+      result: '0'
+    })
+  };
+
+  clear = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="calc-body">
+          <div className="header">
+            <h2>Simple Calculator</h2>
+          </div>
+          <br></br>
+          <ResultComponent result={this.state.result}/>
+          <InputComponent onClick={this.onClick}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
