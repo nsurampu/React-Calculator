f1851db169eb00ded29dc42e3f9b79fd6a7fd939
Naren Surampudi <nsurampu@gmail.com>
2019-05-21 10:53:44 +0530

    Code rectification

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ class App extends Component {
     super();
 
     this.state = {
-      result: '0'
+      result: ''
     }
   }
 
@@ -50,7 +50,7 @@ class App extends Component {
 
   reset = () => {
     this.setState({
-      result: '0'
+      result: ''
     })
   };
 
